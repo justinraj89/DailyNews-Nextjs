28d1fd955165a0fe2bc2c83001ff8508cd625664
justinraj89 <rajewskijustin@gmail.com>
2023-01-25 11:02:02 -0800

    searchBox added to header

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import SearchBox from "./SearchBox";
 //====================================================
 
 function Header() {
@@ -11,9 +12,7 @@ function Header() {
         <Bars3Icon className="h-8 w-8 cursor-pointer" />
         <Link href="/" prefetch={false}>
           <h1 className="font-serif text-4xl text-center">
-            The{' '}
-            <span className="underline decoration-6 decoration-blue-400">Daily</span>
-             {' '}News
+            The Daily News
           </h1>
         </Link>
         <div className="flex items-center justify-end space-x-2">
@@ -24,6 +23,7 @@ function Header() {
         </div>
       </div>
       <NavLinks />
+      <SearchBox />
     </header>
   );
 }
